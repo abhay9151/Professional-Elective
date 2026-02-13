da5f9540dfbb4bcd94f18aa58b73b197e13cd08d
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-02-13 16:30:45 +0530

    Fixed contribution commit

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 // import Header from './Components/header.jsx'
 // import Footer from './Components/footer.jsx'
 // import Registration from './Components/Registration.jsx'
-import EventTodo from './EventTodo';
+// import EventTodo from './EventTodo';
+import Counter from './counter';
 function App() {
   // const y=[2026,2021,2025,2026];
   // const c={name:"Abhay",location:"Ghaziabad",estdyear:2015};
   return(
     <>
     <div>
-    <EventTodo/>
+    {/* <EventTodo/> */}
+    <Counter/>
     </div>
     </>
     
--- project1/src/EventTodo.jsx
+++ project1/src/EventTodo.jsx
@@ -4,36 +4,50 @@ export default function EventToDo()
         alert(`Your Name is ${prop}.`)
     }
 
-    function handleClickList(){
-
+    function handleClickList(e){
+        if(e.target.classList.contains("deleteBtn")){
+            const li = e.target.parentElement;
+            li.remove();
+        }
     }
 
     function handleSubmit(e) {
-        e.preventDefault()  // To prevent page reloading
-        const  input = e.target.task.value;
-        const  inputValue = input.trim();
+        e.preventDefault();  // To prevent page reloading
+
+        const input = e.target.task.value;
+        const inputValue = input.trim();
+
         if(inputValue === ""){
             alert("Task cannot be empty.")
             return;
         }
+
         const li = document.createElement("li");
-        // const d = document.getElementById("taskList")
-        li.innerHTML = `<span>${inputValue}</span>
-        <button class ="deleteBtn">Delete</button>`; 
+
+        li.innerHTML = `
+            <span>${inputValue}</span>
+            <button class="deleteBtn">Delete</button>
+        `; 
+
         document.getElementById("taskList").appendChild(li);
+
+        e.target.task.value = ""; 
     }
 
     return(
         <>
             <h1>To-Do App</h1>
-            <button onClick={()=>handleClick("Arnav Srivastava")}>Click Me</button>
+
+            <button onClick={()=>handleClick("Arnav Srivastava")}>
+                Click Me
+            </button>
 
             <form onSubmit={handleSubmit}>
-                <input type="text" name="task" placeholder="Add your task here"></input>
-                <input type="submit" value={"Add Task"}/>
+                <input type="text" name="task" placeholder="Add your task here" />
+                <input type="submit" value="Add Task"/>
             </form>
+
             <ul id="taskList" onClick={handleClickList}>
-                <li></li>
             </ul>
         </>
     )
